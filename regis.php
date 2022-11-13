7e234d4d34f50301cfcf3e2d550fc4664dd2e2d5
loc <lekimtanloc2002@gmail.com>
2022-11-13 17:08:27 +0700

    code by Loc

--- regis.php
+++ regis.php
@@ -15,21 +15,20 @@
             margin: auto;
         }
         body{
-            background-color: #C1EBF9;
+            background-color: #E2DEF5;
         }
         body>div>form{
-            background-color:#E2DEF5;
+            background-color:#C1EBF9;
             border:1px solid black;
             padding: 30px;
             text-align: center;
             width: 300px;
-            height: 150px;
+            height: 280px;
         }
         body>div>form>button{
             color:white;
             background-color: black;
             padding: 5px;
-            width: 50px;
         }
         body>div>p{
             font-family: Garamond, serif;
@@ -38,7 +37,7 @@
             display: block;
             
         }
-        body>div>form>div>input{
+        .form-control{
             padding: 5px;
             width: 200px;
             
@@ -46,8 +45,9 @@
 
         
     </style>
+
     <div class="container">
-        <p>Login</p>
+        <p>Register</p>
         <form>
             <div class="form-group">
                 <input type="text" class="form-control" id="username" placeholder="Username" name="username">
@@ -57,9 +57,23 @@
                 <input type="password" class="form-control" id="pwd" placeholder="Password" name="pwd">
             </div>
             <br>
-            <button onclick="" type="submit" class="btn btn-default" name="signin">Login</button>
+            <div class="form-group">
+                <input type="password" class="form-control" id="cpwd" placeholder="Comfirm Password" name="cpwd">
+            </div>
+            <br>
+            <label>Please choice role:</label>
+            <div class="form-group">
+                <input type="radio" id="role_company" name="role">
+                <label>Company</label>
+            </div>
+            <div class="form-group">
+                <input type="radio"id="role_employee" name="role">
+                <label>Employee</label>
+            </div>
+            <br>
+            <button onclick="" type="submit" class="btn btn-default" name="signup">Register</button>
             <br><br>
-            <a href="regis.php" name="signup">New user? Create an account.</a>
+            <a href="login.php" name="signin">Already have an account? Sign in.</a>
         </form>
     </div>
 
